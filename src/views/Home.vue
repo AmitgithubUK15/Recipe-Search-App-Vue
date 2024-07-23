88619f08d758f63461379a27e8231911c6c77976
Amit <Amitshah050112003@gmail.com>
2024-07-23 16:16:31 +0530

    Install axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,10 +17,15 @@
 
 <script setup>
 
-import {computed} from 'vue';
+import {computed,onMounted} from 'vue';
 import store from "../store";
+import axiosClient from "../axiosClient.js";
 
-const meals = computed(()=>store.state.meals)
 const letters = 'ABCDEFGHIJKLMNOPQRSTWVXYZ'.split("");
+
+onMounted( async ()=>{
+ const response = await axiosClient.get('/list.php?i=list')
+ console.log(response.data);
+ })
 
 </script>
